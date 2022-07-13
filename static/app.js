aa411716c8d7555d50f66ce3ec64ab2196945e35
GEUNA <geuna0204@gmail.com>
2022-07-13 14:30:36 +0900

    [fix] 비밀번호 확인하는 함수 수정

--- static/app.js
+++ static/app.js
@@ -12,89 +12,80 @@ $(document).ready(() => {
 /* SIGN IN ------------------------------------------------------------------ */
 
 /* SIGN UP ------------------------------------------------------------------ */
-const useremail = document.querySelector('#input-useremail')
-const username = document.querySelector('#input-username')
-const password = document.querySelector('#input-password')
-const password2 = document.querySelector('#input-password2')
+const $useremail = document.querySelector('#input-useremail');
+const $username = document.querySelector('#input-username');
+const $password = document.querySelector('#input-password');
+const $password2 = document.querySelector('#input-password2');
 
-const emailmsg = document.querySelector('#emailmsg')
-const namemsg = document.querySelector('#namemsg')
-const pwmsg1 = document.querySelector('#pwmsg1')
-const pwmsg2 = document.querySelector('#pwmsg2')
+const $emailmsg = document.querySelector('#emailmsg');
+const $namemsg = document.querySelector('#namemsg');
+const $pwmsg1 = document.querySelector('#pwmsg1');
+const $pwmsg2 = document.querySelector('#pwmsg2');
 
-const viewPw = document.querySelector('.view-pw')
+const $viewPw = document.querySelector('.view-pw');
 
-let emailcheck = false
-let pwcheck = false
-let namecheck = false
+let emailcheck = false;
+let pwcheck = false;
+let namecheck = false;
 
 function sign_up() {
-
-  if (useremail.value === "") {
-    emailmsg.innerText = "이메일을 입력해주세요"
-
-  } else if (!is_useremail(useremail.value)) {
-    emailmsg.innerText = "이메일 형식을 확인해주세요."
-
+  if ($useremail.value === '') {
+    $emailmsg.innerText = '이메일을 입력해주세요';
+  } else if (!is_useremail($useremail.value)) {
+    $emailmsg.innerText = '이메일 형식을 확인해주세요.';
   } else {
-    emailmsg.innerText = "사용가능한 이메일 입니다"
-    emailcheck = true
+    $emailmsg.innerText = '사용가능한 이메일 입니다';
+    emailcheck = true;
   }
 
-  if (username.value === "") {
-    namemsg.innerText = "닉네임을 입력해주세요"
-
-  } else if (is_username(username.value)) {
-    namemsg.innerText = "공백 및 특수문자는 사용이 불가합니다."
-
-  } else if (is_username2(username.value)) {
-    namemsg.innerText = "공백 및 특수문자는 사용이 불가합니다."
-
+  if ($username.value === '') {
+    $namemsg.innerText = '닉네임을 입력해주세요';
+  } else if (is_username($username.value)) {
+    $namemsg.innerText = '공백 및 특수문자는 사용이 불가합니다.';
+  } else if (is_username2($username.value)) {
+    $namemsg.innerText = '공백 및 특수문자는 사용이 불가합니다.';
   } else {
-    namemsg.innerText = "사용가능한 닉네임 입니다"
-    namecheck = true
+    $namemsg.innerText = '사용가능한 닉네임 입니다';
+    namecheck = true;
   }
 
-  if (password.value === "") {
-    pwmsg1.innerText = "비밀번호를 입력해주세요"
-
-  } else if (!is_password(password.value)) {
-    pwmsg1.innerText = "비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자"
-
+  if ($password.value === '') {
+    $pwmsg1.innerText = '비밀번호를 입력해주세요';
+  } else if (!is_password($password.value)) {
+    $pwmsg1.innerText =
+      '비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자';
   } else {
-    pwmsg1.innerText = "사용가능한 비밀번호 입니다."
-
+    $pwmsg1.innerText = '사용가능한 비밀번호 입니다.';
   }
-  if (password2.value === "") {
-    pwmsg2.innerText = "확인용 비밀번호를 입력해주세요"
-
-  } else if (password2.value !== password.value) {
-    pwmsg2.innerText = "비밀번호가 일치하지 않습니다."
-
+  if ($password2.value === '') {
+    $pwmsg2.innerText = '확인용 비밀번호를 입력해주세요';
+  } else if ($password2.value !== $password.value) {
+    $pwmsg2.innerText = '비밀번호가 일치하지 않습니다.';
   } else {
-    pwmsg2.innerText = "비밀번호가 일치합니다."
-    pwcheck = true
+    $pwmsg2.innerText = '비밀번호가 일치합니다.';
+    pwcheck = true;
   }
 
   if (emailcheck === true && pwcheck === true && namecheck === true) {
     $.ajax({
-      type: "POST",
-      url: "/signup/save",
+      type: 'POST',
+      url: '/signup/save',
       data: {
-        useremail_give: useremail.value,
-        username_give: username.value,
-        password_give: password.value
+        useremail_give: $useremail.value,
+        username_give: $username.value,
+        password_give: $password.value,
       },
       success: function (response) {
-        alert(response['msg'])
-        window.location.replace("/")
-      }
+        alert(response['msg']);
+        window.location.replace('/');
+      },
     });
   }
 }
 
 function is_useremail(asValue) {
-  const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  const regExp =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
   return regExp.test(asValue);
 }
 
@@ -104,19 +95,22 @@ function is_password(asValue) {
 }
 
 function is_username(asValue) {
-  const regExp = /[`~!@#$%^&*|\\\'\";:\/?]/gi
+  const regExp = /[`~!@#$%^&*|\\\'\";:\/?]/gi;
   return regExp.test(asValue);
 }
 
 function is_username2(asValue) {
-  const regExp = /\s/g
+  const regExp = /\s/g;
   return regExp.test(asValue);
 }
 
 function view_pw(event) {
-  if (event.parentElement.children[0].type === "password") {
-    event.parentElement.children[0].type = "text"
-  } else event.parentElement.children[0].type = "password"
+  let target = event.parentElement.children[0];
+  if (target.type === 'password') {
+    target.type = 'text';
+  } else {
+    target.type = 'password';
+  }
 }
 
 const goToSignUp = () => {
